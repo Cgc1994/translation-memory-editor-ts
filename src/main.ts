97d2f4cf1880a831561b1a241a9301650b9a3522
Carlos González <carlos.gonzalez@hispatec.com>
2024-01-21 13:17:21 +0100

    delete types folder

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue';
 import App from '/@/App.vue';
 import ElementPlus from 'element-plus';
-import VueGridLayout from 'vue-grid-layout';
 
 const app = createApp(App);
 
-app.use(ElementPlus).use(VueGridLayout).mount('#app');
+app.use(ElementPlus).mount('#app');
